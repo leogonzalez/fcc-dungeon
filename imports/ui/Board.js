a194c7d7195bff6f65ef408af1052a30dde19b9a
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2017-08-06 17:32:03 -0300

    new moving system

--- imports/ui/Board.js
+++ imports/ui/Board.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import Tile from './Tile.js';
 
-const board = [...Array(306)].map((item,i) => i);
+const squares = 306;
+const rows = 306/18;
+const columns = 306/rows;
+const board = [...Array(squares)].map((item,i) => i);
 
 export default class Board extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      playerPos:25
+      playerPos:{
+        X: 5,
+        Y: 5
+      }
     };
     this.movePlayer = this.movePlayer.bind(this);
   }
   movePlayer(event){
     switch (event.key) {
       case 'ArrowUp':
-        this.setState((prevState) => ({playerPos:prevState.playerPos -18}));
+        this.setState((prevState) => ({playerPos:{
+          Y:prevState.playerPos.Y - 1?,
+          X:prevState.playerPos.X
+        }}));
         break;
       case 'ArrowDown':
-        this.setState((prevState) => ({playerPos:prevState.playerPos +18}));
+      this.setState((prevState) => ({playerPos:{
+        Y:prevState.playerPos.Y + 1,
+        X:prevState.playerPos.X
+      }}));
         break;
       case 'ArrowLeft':
-        this.setState((prevState) => ({playerPos:prevState.playerPos -1}));
+      this.setState((prevState) => ({playerPos:{
+        Y:prevState.playerPos.Y,
+        X:prevState.playerPos.X -1
+      }}));
         break;
       case 'ArrowRight':
-        this.setState((prevState) => ({playerPos:prevState.playerPos +1}));
+      this.setState((prevState) => ({playerPos:{
+        Y:prevState.playerPos.Y,
+        X:prevState.playerPos.X +1
+      }}));
         break;
     }
   }
@@ -34,7 +52,9 @@ export default class Board extends React.Component {
 
       {
         board.map((x,i) => {
-            return (<Tile key={i} pos={i} player={i==this.state.playerPos}/>)
+          const Y=Math.floor(i/columns);
+          const X=i%columns;
+            return (<Tile key={i} pos={`${Y}|${X}`} player={X==this.state.playerPos.X && Y==this.state.playerPos.Y}/>)
         })
       }
 
